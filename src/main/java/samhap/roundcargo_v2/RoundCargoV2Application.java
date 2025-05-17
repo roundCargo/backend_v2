97a2d6d9c9ff9dda5513a56c224b3c187e0db246
Gihyeon <ryuoo0123@gmail.com>
2025-05-17 21:41:58 +0900

    inital commit

---
+++ src/main/java/samhap/roundcargo_v2/RoundCargoV2Application.java
@@ -0,0 +1,13 @@
+package samhap.roundcargo_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoundCargoV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoundCargoV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/samhap/roundcargo_v2/RoundCargoV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package samhap.roundcargo_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoundCargoV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
